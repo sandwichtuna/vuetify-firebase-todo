72e5e01b6c0da0a876dbebb0d5293906834112b5
max <sumeth.sir.media@gmail.com>
2021-08-13 15:37:22 +0700

    add Navigation drawer and Login dialog.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app color="primary" dark>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
@@ -15,47 +13,78 @@
           width="40"
         />
 
-        <v-img
+        <!-- <v-img
           alt="Vuetify Name"
           class="shrink mt-1 hidden-sm-and-down"
           contain
           min-width="100"
           src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
           width="100"
-        />
+        /> -->
       </div>
 
       <v-spacer></v-spacer>
-
-      <!-- <v-btn text router to="/todos">
-        <span class="mr-2">Login</span>
-      </v-btn> -->
-
-      <!-- <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn> -->
+      <div><LoginDialog /></div>
     </v-app-bar>
 
+    <v-navigation-drawer
+      color="blue darken-4"
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list>
+        <v-list-item-group v-model="group">
+          <v-list-item
+            v-for="link in links"
+            :key="link.name"
+            route
+            :to="link.route"
+          >
+            <v-icon left class="white--text">{{ link.icon }}</v-icon>
+            <v-list-item-title class="white--text">{{
+              link.name
+            }}</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+      <template v-slot:append>
+        <div class="pa-2">
+          <v-btn block text dark>
+            Logout
+          </v-btn>
+        </div>
+      </template>
+    </v-navigation-drawer>
+
     <v-main class="mt-4">
-      <router-view/>
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
+import LoginDialog from './components/LoginDialog.vue';
 
 export default {
   name: 'App',
 
   data: () => ({
-    //
+    links: [
+      { name: 'Dashboard', icon: 'mdi-view-dashboard', route: '/' },
+      { name: 'About', icon: 'mdi-information-outline', route: '/about' },
+      { name: 'Admin', icon: 'mdi-account-supervisor', route: '/admin' },
+    ],
+    drawer: false,
+    group: null,
   }),
+  watch: {
+    group() {
+      this.drawer = false;
+    },
+  },
   components: {
+    LoginDialog,
   },
 };
 </script>
---
+++ src/components/LoginDialog.vue
@@ -0,0 +1,69 @@
+<template>
+  <v-dialog transition="dialog-bottom-transition" max-width="700">
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn text v-bind="attrs" v-on="on"
+        ><span class="text-body-1">Login</span>
+        <v-icon right>mdi-login-variant</v-icon>
+      </v-btn>
+    </template>
+    <template v-slot:default="dialog">
+      <v-card>
+        <v-row justify="center">
+          <v-col cols="12">
+            <v-toolbar color="primary" dark>Login</v-toolbar>
+          </v-col>
+          <v-col cols="12"></v-col>
+          <v-col cols="10">
+            <v-text-field
+              solo
+              label="email"
+              prepend-icon="mdi-account"
+              v-model="user.email"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="10">
+            <v-text-field
+              solo
+              label="password"
+              type="password"
+              prepend-icon="mdi-lock"
+              v-model="user.password"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12">
+            <v-card-actions class="justify-space-around">
+              <v-btn color="primary" @click="dialog.value = false">
+                Login
+              </v-btn>
+              <v-btn color="error" @click="clear"> Clear </v-btn>
+            </v-card-actions>
+          </v-col>
+        </v-row>
+      </v-card>
+    </template>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        email: '',
+        password: '',
+      },
+    };
+  },
+
+  methods: {
+    clear() {
+      this.user.email = '';
+      this.user.password = '';
+      // this.$router.push('/todos');
+    },
+  },
+};
+</script>
+
+<style>
+</style>
